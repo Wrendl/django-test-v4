5c5b367cf534a3688794d1cd43aff8866d9e9bb1
Nariman <narimanganiyev26@gmail.com>
2022-03-22 19:53:01 +0600

    1

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -59,8 +59,7 @@ ROOT_URLCONF = 'auth_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'build')],
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    (os.path.join(BASE_DIR, 'build/static'))
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
