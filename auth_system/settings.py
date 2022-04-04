bf102533f5feac5acc667ae86de57a0c874c9d06
Nariman <narimanganiyev26@gmail.com>
2022-04-04 21:33:43 +0600

    v1.05

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -198,11 +198,13 @@ AUTH_USER_MODEL = 'accounts.UserAccount'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'https://enigmatic-temple-06724.herokuapp.com',
-]
+CORS_ORIGIN_ALLOW_ALL=True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',
+#     'http://127.0.0.1:8000',
+#     'https://enigmatic-temple-06724.herokuapp.com',
+# ]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
