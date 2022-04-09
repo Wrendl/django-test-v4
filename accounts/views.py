92551667844860d3cb2320f6c65e2dd32438ca98
Nariman <narimanganiyev26@gmail.com>
2022-04-09 14:39:11 +0600

    v1.091

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,12 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from .models import UserAccount
 from .serializers import UserCreateSerializer
-from rest_framework.decorators import api_view
 
 
 def index(request):
     return render(request, 'accounts/index.html')
 
 
-@api_view(['GET'])
 def confirm(request, uid, token):
     if request.method == 'GET':
         return render(request, 'accounts/confirm.html', {'uid': uid, 'token': token})
